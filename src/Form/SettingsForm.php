57e43365e445d53a1a91e31a85adc702b1a95da7
Mikkel Ricky <rimi@aarhus.dk>
2021-07-05 21:40:34 +0200

    Added more settings

--- src/Form/SettingsForm.php
+++ src/Form/SettingsForm.php
@@ -2,14 +2,58 @@
 
 namespace Drupal\adgangsstyring\Form;
 
+use Drupal\Core\Config\ConfigFactoryInterface;
+use Drupal\Core\Entity\EntityTypeManager;
 use Drupal\Core\Form\ConfigFormBase;
 use Drupal\Core\Form\FormStateInterface;
+use Symfony\Component\DependencyInjection\ContainerInterface;
 
 /**
  * Settings form.
  */
 class SettingsForm extends ConfigFormBase {
   public const SETTINGS = 'adgangsstyring.settings';
+
+  /**
+   * The user storage.
+   *
+   * @var \Drupal\Core\Entity\EntityStorageInterface|mixed|object
+   */
+  private $userStorage;
+
+  /**
+   * The user role storage.
+   *
+   * @var \Drupal\Core\Entity\EntityStorageInterface|mixed|object
+   */
+  private $roleStorage;
+
+  /**
+   * SettingsForm constructor.
+   *
+   * @param \Drupal\Core\Config\ConfigFactoryInterface $configFactory
+   *   The config factory.
+   * @param \Drupal\Core\Entity\EntityTypeManager $entityTypeManager
+   *   The entity type manager.
+   *
+   * @throws \Drupal\Component\Plugin\Exception\InvalidPluginDefinitionException
+   * @throws \Drupal\Component\Plugin\Exception\PluginNotFoundException
+   */
+  public function __construct(ConfigFactoryInterface $configFactory, EntityTypeManager $entityTypeManager) {
+    parent::__construct($configFactory);
+    $this->userStorage = $entityTypeManager->getStorage('user');
+    $this->roleStorage = $entityTypeManager->getStorage('user_role');
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public static function create(ContainerInterface $container) {
+    return new static(
+      $container->get('config.factory'),
+      $container->get('entity_type.manager')
+    );
+  }
 
   /**
    * {@inheritdoc}
@@ -27,45 +71,69 @@ class SettingsForm extends ConfigFormBase {
     // Default settings.
     $config = $this->config(self::SETTINGS);
 
-    $form['client_id'] = [
-      '#type' => 'textfield',
-      '#title' => $this->t('Client id'),
-      '#default_value' => $config->get('client_id'),
-      '#description' => $this->t('The client id.'),
-      '#required' => TRUE,
+    $form['api_settings'] = [
+      '#type' => 'fieldset',
+      '#title' => $this->t('API settings'),
+
+      'client_id' => [
+        '#type' => 'textfield',
+        '#title' => $this->t('Client id'),
+        '#default_value' => $config->get('client_id'),
+        '#description' => $this->t("The client id. Should be set in <code>settings.local.php</code>: <code>\$config['adgangsstyring.settings']['client_id'] = '…';</code>"),
+        '#required' => TRUE,
+      ],
+
+      'client_secret' => [
+        '#type' => 'textfield',
+        '#title' => $this->t('Client secret'),
+        '#default_value' => $config->get('client_secret'),
+        '#description' => $this->t("The client secret. Should be set in <code>settings.local.php</code>: <code>\$config['adgangsstyring.settings']['client_secret'] = '…';</code>"),
+        '#required' => TRUE,
+      ],
+
+      'group_id' => [
+        '#type' => 'textfield',
+        '#title' => $this->t('Group id'),
+        '#default_value' => $config->get('group_id'),
+        '#description' => $this->t("The group id. Should be set in <code>settings.local.php</code>: <code>\$config['adgangsstyring.settings']['group_id'] = '…';</code>"),
+        '#required' => TRUE,
+      ],
+
+      'tenant_id' => [
+        '#type' => 'textfield',
+        '#title' => $this->t('Tenant id'),
+        '#default_value' => $config->get('tenant_id'),
+        '#description' => $this->t("The tenant id. Should be set in <code>settings.local.php</code>: <code>\$config['adgangsstyring.settings']['tenant_id'] = '…';</code>"),
+        '#required' => TRUE,
+      ],
     ];
 
-    $form['client_secret'] = [
-      '#type' => 'textfield',
-      '#title' => $this->t('Client secret'),
-      '#default_value' => $config->get('client_secret'),
-      '#description' => $this->t('The client secret.'),
-      '#required' => TRUE,
+    $form['exclusions'] = [
+      '#type' => 'fieldset',
+      '#title' => $this->t('Exclusions'),
     ];
 
-    $form['group_id'] = [
-      '#type' => 'textfield',
-      '#title' => $this->t('Group id'),
-      '#default_value' => $config->get('group_id'),
-      '#description' => $this->t('The group id.'),
-      '#required' => TRUE,
+    $options = [];
+    foreach ($this->roleStorage->loadMultiple() as $role) {
+      $options[$role->id()] = $role->label();
+    }
+    $form['exclusions']['excluded_roles'] = [
+      '#title' => $this->t('Excluded roles'),
+      '#type' => 'checkboxes',
+      '#options' => $options,
+      '#default_value' => $config->get('excluded_roles') ?: [],
     ];
 
-    $form['tenant_id'] = [
-      '#type' => 'textfield',
-      '#title' => $this->t('Tenant id'),
-      '#default_value' => $config->get('tenant_id'),
-      '#description' => $this->t('The tenant id.'),
-      '#required' => TRUE,
+    $form['exclusions']['excluded_users'] = [
+      '#title' => $this->t('Excluded users'),
+      '#type' => 'entity_autocomplete',
+      '#target_type' => 'user',
+      '#default_value' => $this->userStorage->loadMultiple($config->get('excluded_users') ?: [-1]),
+      '#tags' => TRUE,
+      '#description' => $this->t('Separate by comma.'),
     ];
 
     return $form;
-  }
-
-  /**
-   * {@inheritdoc}
-   */
-  public function validateForm(array &$form, FormStateInterface $form_state) {
   }
 
   /**
@@ -77,6 +145,8 @@ class SettingsForm extends ConfigFormBase {
     $config->set('client_secret', $form_state->getValue('client_secret'));
     $config->set('group_id', $form_state->getValue('group_id'));
     $config->set('tenant_id', $form_state->getValue('tenant_id'));
+    $config->set('excluded_roles', $form_state->getValue('excluded_roles'));
+    $config->set('excluded_users', array_column($form_state->getValue('excluded_users') ?? [], 'target_id'));
     $config->save();
 
     return parent::submitForm($form, $form_state);
